8d3f9333990a7c8f8ae5720142b5780056ab20c0
szoti <evildevil21>
2022-05-15 15:23:42 +0200

    Final exam - 2013 oct.

---
+++ src/com/finalexam/roadsideChecks/Data.java
@@ -0,0 +1,28 @@
+package com.finalexam.roadsideChecks;
+import java.time.LocalTime;
+
+public class Data {
+    private LocalTime localTime;
+    private String licensePlateNumber;
+
+    public Data(LocalTime localTime, String licensePlateNumber) {
+        this.localTime = localTime;
+        this.licensePlateNumber = licensePlateNumber;
+    }
+    
+    public LocalTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(LocalTime localTime) {
+        this.localTime = localTime;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+}
