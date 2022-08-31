399103835e98595d969dfea75d2b46a4f1446bd5
spangli <szoti13@gmail.com>
2022-08-31 13:51:07 +0200

    Final exam - 2010 maj.

---
+++ src/main/java/site/SiteMain.java
@@ -0,0 +1,55 @@
+package site;
+
+import common.GeneralFileReader;
+
+import java.util.List;
+
+public class SiteMain {
+    public static void main(String[] args) {
+
+        final int FIRST_NUMBER_OF_GOOD_LIFE = 2;
+
+        GeneralFileReader reader = new GeneralFileReader("src/main/resources/site/telkek.txt");
+
+        List<String> inputData = reader.readLines();
+        SiteManager manager = new SiteManager(inputData);
+
+        // 2.task
+        int district = manager.getStreetsDistrict();
+        System.out.println("\n2.task \nA körbesétáláshoz " + district + " métert kell megtenni" );
+
+        // 3.task
+        long allSite = manager.getAllSiteWichTwentyMeterOrSmaller();
+        System.out.println("\n3.task \nA összesen " + allSite + " telket kell beépiteni" );
+
+        // 4.task
+        SiteData smallest = manager.getSmallestSiteOnTheRichRow();
+        SiteData biggest = manager.getBiggestSiteOnTheRichRow();
+
+        System.out.println("\n4.task \nA legkisebb telek " + smallest.getHouseNumber() + " " + smallest.getWidth() * smallest.getLength()
+                           + "\nA legnagyobb telek  " + biggest.getHouseNumber() +" "+ biggest.getWidth() * biggest.getLength()
+                            + "\nA két telek közti telkek száma " + (biggest.getHouseNumber() - smallest.getHouseNumber()) );
+
+
+        // 5.task
+        int amount = manager.getTaxOfTheRichRow();
+        System.out.println("\n5.task \nA összesen " + amount + " adóbevételre  számíthat az önkormányzat" );
+
+        // 6.task
+        System.out.println("\n6.task");
+        List<SiteData> threeSite = manager.getLastThreeSiteOfGoodLifeRow();
+        for (SiteData site : threeSite) {
+            System.out.println("A házszám " + site.getHouseNumber() + " távolsága " +
+                    (site.getHouseNumber()-FIRST_NUMBER_OF_GOOD_LIFE));
+        }
+
+        // 7.task
+        System.out.println("\n7.task ");
+        List<SiteData> list = manager.getAllSiteLengthOfGoodLife();
+        for (SiteData site : list) {
+            System.out.println(site);
+        }
+
+
+    }
+}
---
+++ src/test/groovy/site/SiteManagerTest.groovy
@@ -0,0 +1,188 @@
+package site
+
+import spock.lang.Specification
+
+class SiteManagerTest extends Specification {
+
+
+    def 'parseInput'() {
+        setup:
+            def list = ['','1 23 21 33', '2 12 44 55', '3 23 21 66']
+
+            def expected = [['houseNumber' : 1, 'width' : 23, 'length' : 21, 'area' : 483],
+                            ['houseNumber' : 2, 'width' : 12, 'length' : 44, 'area' : 528],
+                            ['houseNumber' : 3, 'width' : 23, 'length' : 21, 'area' : 483]]
+
+        when:
+            def manager = new SiteManager(list)
+        then:
+            assert manager.allSite.size() == 3
+
+            for (int i = 1; i < list.size()-1; i++) {
+                validateSite(manager.allSite.get(i), expected.get(i))
+            }
+    }
+
+
+    def 'getStreetsDistrict'() {
+        setup:
+            def manager = new SiteManager(['', '1 23 21 33', '2 ' + test + ' 44 55', '3 23 21 66'])
+        when:
+            def number = manager.getStreetsDistrict()
+        then:
+            assert number == expected
+        where:
+            test           | expected
+            20             | 200
+            30             | 220
+            0              | 160
+    }
+
+
+    def 'getAllSiteWitchTwentyMeterOrSmaller'() {
+        setup:
+            def manager = new SiteManager([])
+            manager.goodLifeRow = list
+
+        when:
+            def number = manager.getAllSiteWichTwentyMeterOrSmaller()
+        then:
+            assert number == expected
+        where:
+            list                        | expected
+            [new SiteData(2,10,19,30),
+             new SiteData(3,10,21,30)]  | 2
+            [ new SiteData(3,10,20,30)] | 1
+            []                          | 0
+    }
+
+
+    def 'getBiggestSiteOnTheRichRow'() {
+        setup:
+            def manager = new SiteManager([])
+            manager.richRow = [new SiteData(2,10,19,30),
+                               new SiteData(3,33,21,40),
+                               new SiteData(4,21,21,40)]
+
+        when:
+            def number = manager.getBiggestSiteOnTheRichRow()
+        then:
+            assert number.houseNumber == 3
+            assert number.width == 33
+    }
+
+
+    def 'getSmallestSiteOnTheRichRow'() {
+        setup:
+            def manager = new SiteManager([])
+            manager.richRow = [new SiteData(2,21,19,31),
+                               new SiteData(3,19,21,30),
+                               new SiteData(4,44,19,31),
+                               new SiteData(6,43,21,30)]
+
+        when:
+            def number = manager.getSmallestSiteOnTheRichRow()
+        then:
+            assert number.houseNumber == 3
+            assert number.width == 19
+    }
+
+
+    def 'getTaxOfTheRichRow'() {
+        setup:
+            def manager = new SiteManager([])
+            manager.richRow = test as List<SiteData>
+
+        when:
+            def number = manager.getTaxOfTheRichRow()
+        then:
+            assert number == expected
+        where:
+            test                                   |  expected
+            [new SiteData(2,10,19,700)]  | 28600
+            [new SiteData(2,10,19,1200)] | 38100
+            [new SiteData(2,10,19,700),
+             new SiteData(2,10,19,1200)] | 66600
+    }
+
+    def 'getLastThreeSiteOfGoodLifeRow'() {
+
+        setup:
+            def manager = new SiteManager([])
+            manager.goodLifeRow = [new SiteData(1,10,19,31),
+                                   new SiteData(2,20,21,30),
+                                   new SiteData(3,30,20,30),
+                                   new SiteData(4,23,19,180),
+                                   new SiteData(5,23,13,300),
+                                   new SiteData(6,19,22,220)]
+
+        when:
+            def number = manager.getLastThreeSiteOfGoodLifeRow()
+
+        then:
+            assert number.size() == 3
+            assert number.get(0).houseNumber == 4
+            assert number.get(0).width == 23
+            assert number.get(0).length == 19
+            assert number.get(0).area == 180
+            assert number.get(1).houseNumber == 5
+            assert number.get(1).width == 23
+            assert number.get(1).length == 13
+            assert number.get(1).area == 300
+            assert number.get(2).houseNumber == 6
+            assert number.get(2).width == 19
+            assert number.get(2).length == 22
+            assert number.get(2).area == 220
+    }
+
+
+    def 'getAllSiteLengthOfGoodLife'(){
+
+        setup:
+            def list = ['', '1 23 23 33',
+                            '2 12 0 55',
+                            '3 23 30 66',
+                            '4 23 0 33',
+                            '5 12 44 55',
+                            '6 23 0 66']
+            def manager = new SiteManager(list)
+
+            def expected = [new SiteData(2,12,47,0),
+                            new SiteData(4,23,40,0),
+                            new SiteData(6,23,26,0)]
+
+        when:
+            def test = manager.getAllSiteLengthOfGoodLife()
+
+        then:
+            assert test.get(0).houseNumber == expected.get(0).houseNumber
+            assert test.get(0).width == expected.get(0).width
+            assert test.get(0).length == expected.get(0).length
+            assert test.get(0).area == expected.get(0).area
+
+            assert test.get(1).houseNumber == expected.get(1).houseNumber
+            assert test.get(1).width == expected.get(1).width
+            assert test.get(1).length == expected.get(1).length
+            assert test.get(1).area == expected.get(1).area
+
+            assert test.get(2).houseNumber == expected.get(2).houseNumber
+            assert test.get(2).width == expected.get(2).width
+            assert test.get(2).length == expected.get(2).length
+            assert test.get(2).area == expected.get(2).area
+
+    }
+
+
+
+    static def validateSite(SiteData site, def expectedData){
+        assert site.houseNumber == expectedData.houseNumber
+        assert site.width == expectedData.width
+        assert site.length == expectedData.length
+        assert site.area == expectedData.area
+        return true
+    }
+}
+
+
+
+
